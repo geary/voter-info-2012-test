1c142060bc9088163f7694ffedd14da6f5ae85cc
Michael Geary <mg@mg.to>
2010-08-20 18:06:43 -0700

    Add some commented-out test code

--- appengine/voter-info-gadget.py
+++ appengine/voter-info-gadget.py
@@ -7,21 +7,41 @@
 #
 #print xml
 
+#import re
+#from pprint import pformat, pprint
+
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
+#def dumpRequest( req ):
+#	return pformat({
+#		'environ': req.environ,
+#		'url': req.url,
+#		'headers': req.headers,
+#	})
+
+#def addDump( xml, req ):
+#	dr = dumpRequest( req )
+#	dr = re.sub( r'\}', '\n}', dr )
+#	dr = re.sub( r"'wsgi[^\n]+\n", '', dr )
+#	dr = re.sub ( r'\n\s*', ' ', dr )
+#	dr = re.sub ( r',\s*\}', '}', dr )
+#	return xml.replace( 'var opt =', 'alert( (' + dr + ').toSource() );\n\n\tvar opt =' )  # poor man's template
+	
 class GadgetHandler( webapp.RequestHandler ):
-	def get( self, debug ):
+	def get( self, dump, debug ):
 		self.response.headers['Content-Type'] = 'application/xml'
 		if debug == None: debug = ''
 		f = open( 'voter-info-gadget.xml', 'r' )
 		xml = f.read()
-		xml = xml.replace( '{{debug}}', debug )  # poor man's template
 		f.close()
+		xml = xml.replace( '{{debug}}', debug )  # poor man's template
+		#if dump:
+		#	xml = addDump( xml, self.request )
 		self.response.out.write( xml )
 
 application = webapp.WSGIApplication([
-	( r'/(.+)?voter-info-gadget\.xml', GadgetHandler )
+	( r'/(dump-)?(.+)?voter-info-gadget\.xml', GadgetHandler )
 ], debug = True )
 
 def main():
