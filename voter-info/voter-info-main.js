ed54861cd145c0a248afc71b2c5eb28b312e628c
Michael Geary <mg@mg.to>
2010-10-01 14:43:38 -0700

    Fix broken gadget: turn off candidate list temporarily

--- voter-info/voter-info-main.js
+++ voter-info/voter-info-main.js
@@ -985,6 +985,8 @@ function gadgetReady() {
 		}
 		
 		function candidates() {
+			return '';  // TEMP: turn this off until contest data available
+			// OLD CODE:
 			var contests = vote && vote.poll && vote.poll.contests && vote.poll.contests[0];
 			if( ! contests  ||  ! contests.length ) return '';
 			contests = sortArrayBy( contests, 'ballot_placement', { numeric:true } );
