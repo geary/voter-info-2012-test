874451e355d4ca2216fc487df8a4009f1c86851a
Michael Geary <mg@mg.to>
2011-07-26 00:01:06 -0700

    Update to jQuery 1.6.2

--- voter-info/voter-info.js
+++ voter-info/voter-info.js
@@ -3,7 +3,7 @@
 // See UNLICENSE or http://unlicense.org/ for public domain notice.
 
 document.write(
-	'<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery', opt.debug ? '' : '.min', '.js">',
+	'<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery', opt.debug ? '' : '.min', '.js">',
 	'</script>'
 );
 
