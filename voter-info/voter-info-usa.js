d8af79fbce8366ba66ec2c374c84b31a2107d84c
Michael Geary <mg@mg.to>
2011-08-23 12:34:58 -0700

    Fix bug with missing contests

--- voter-info/voter-info-usa.js
+++ voter-info/voter-info-usa.js
@@ -634,7 +634,7 @@ function setVoteHtml() {
 function getContests() {
 	var contests = vote && vote.poll && vote.poll.contests;
 	// TEMP FOR API BUG
-	if( contests[0]  &&  contests[0].length )
+	if( contests  &&  contests[0]  &&  contests[0].length )
 		contests = contests[0];
 	// END TEMP
 	return contests && contests.length && contests;
