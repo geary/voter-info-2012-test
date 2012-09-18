457ec0d6b7d746623252418a10d63f914e83973e
Michael Geary <mg@mg.to>
2012-09-17 23:15:21 -0700

    Make it a normal HTML page instead of a gadget

--- voter-info/voter-info.js
+++ voter-info/voter-info.js
@@ -2,6 +2,15 @@
 // By Michael Geary - http://mg.to/
 // See UNLICENSE or http://unlicense.org/ for public domain notice.
 
+var opt = {
+	writeScript: function( name, seconds ) {
+		document.write(
+			'<script type="text/javascript" src="', name, '">',
+			'<\/script>'
+		);
+	}
+};
+
 document.write(
 	'<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery', opt.debug ? '' : '.min', '.js">',
 	'</script>'
